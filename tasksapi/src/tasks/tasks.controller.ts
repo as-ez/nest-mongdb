d049f744788e56d21e81e1368c0f3b1c5819cde3
as-ez <astradaezequiel.tpi@gmail.com>
2023-07-29 15:59:37 -0300

    creada logica controllers

--- tasksapi/src/tasks/tasks.controller.ts
+++ tasksapi/src/tasks/tasks.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Put, Body, Param } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from 'src/dto/create-task.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private tasksService: TasksService) {}
+
+@Get()
+    findAll(){
+        return this.tasksService.findAll();
+}
+
+@Get(':id')
+    findOne(@Param('id') id: string){
+        return this.tasksService.findOne(id)
+}
+
+
+@Post()
+    create(@Body() body: CreateTaskDto){
+        return this.tasksService.create(body)
+}
+
+@Delete(':id')
+    delete(@Param('id') id: string){
+        return this.tasksService.delete(id)
+}
+
+@Put(':id')
+    update(@Param('id') id:string, @Body() body:any){
+        return this.tasksService.update(id, body)
+}
+}
