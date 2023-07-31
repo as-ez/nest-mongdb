1a1e54956da2edc206e256aa51a8d9e4d147d934
as-ez <astradaezequiel.tpi@gmail.com>
2023-07-31 20:27:50 -0300

    React setup

---
+++ tasksfront/src/App.tsx
@@ -0,0 +1,16 @@
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+
+function App() {
+  return (
+    <div className="bg-zinc-900 h-screen text-white flex items-center justify-center">
+      <div className="bg-gray-950 p-4 w-2/5">
+        <h1 className="text-3xl font-bold text-center block my-2">Task App</h1>
+        <TaskList />
+        <TaskForm />
+      </div>
+    </div>
+  )
+}
+
+export default App
