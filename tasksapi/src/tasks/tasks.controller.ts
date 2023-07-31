1f079115aff141f4455b88707431ce89698f5fb1
as-ez <astradaezequiel.tpi@gmail.com>
2023-07-31 20:08:37 -0300

    HTTP Status

--- tasksapi/src/tasks/tasks.controller.ts
+++ tasksapi/src/tasks/tasks.controller.ts
@@ -1,34 +1,49 @@
-import { Controller, Get, Post, Delete, Put, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Put, Body, Param, ConflictException, NotFoundException, HttpCode} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from 'src/dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService) {}
+    constructor(private tasksService: TasksService) { }
 
-@Get()
-    findAll(){
+    @Get()
+    findAll() {
         return this.tasksService.findAll();
-}
+    }
 
-@Get(':id')
-    findOne(@Param('id') id: string){
-        return this.tasksService.findOne(id)
-}
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        const task = await this.tasksService.findOne(id)
+        if (!task) throw new NotFoundException('Task not found');
+        return task;
+    }
 
 
-@Post()
-    create(@Body() body: CreateTaskDto){
-        return this.tasksService.create(body)
-}
+    @Post()
+    async create(@Body() body: CreateTaskDto) {
+        try {
+            return await this.tasksService.create(body)
+        } catch (error) {
+            if (error.code === 11000) {
+                throw new ConflictException('Task already exists')
+            }
+            throw error;
+        }
 
-@Delete(':id')
-    delete(@Param('id') id: string){
-        return this.tasksService.delete(id)
-}
+    }
 
-@Put(':id')
-    update(@Param('id') id:string, @Body() body:any){
-        return this.tasksService.update(id, body)
-}
+    @Delete(':id')
+    @HttpCode(204)
+    async delete(@Param('id') id: string) {
+        const task = await this.tasksService.delete(id)
+        if (!task) throw new NotFoundException('Task not found');
+        return task;
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() body: any) {
+        const task = await this.tasksService.update(id, body)
+        if (!task) throw new NotFoundException('Task not found');
+        return task;
+    }
 }
